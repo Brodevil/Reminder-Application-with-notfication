68e1c37b89f7650879a7fa8d179e5bf0e6df5891
Brodevil <brodevil89@gmail.com>
2021-04-25 16:18:59 +0530

    Are yar nahi hoo raha kasam se

--- main.py
+++ main.py
@@ -12,15 +12,21 @@ def readExel(path):
     sheet_obj = wb_obj.active
     max_col = sheet_obj.max_column
     
-    records = dict()
+    records = list()
 
     # Loop will print all columns name
     for j in range(1, sheet_obj.max_row +1):
         for i in range(1, max_col + 1):
-            row_obj = sheet_obj.cell(row =j , column = i)
-
-            records.update({row_obj.value: row_obj.value})
-
+            # row_obj = sheet_obj.cell(row =j , column = i)
+            a = sheet_obj.cell(row=j, column=i)
+            print(a.value)
+            records.append(a.value)
+    print(records)
+    records = [[i, j] for index, i in enumerate(records) for num, j in enumerate(records) if index % 2 != 1  if num % 2!= 0]
+    print(records)
+    for index, value in enumerate(records):
+        if index % 2 != 0:
+            del records[index]
     print(records)
 
 
