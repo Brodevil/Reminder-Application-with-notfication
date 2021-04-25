c6ec5e498487281fa1a4f2400c355c13d32790f5
Brodevil <brodevil89@gmail.com>
2021-04-25 15:43:46 +0530

    Now we will use openpyxl moduel to read the excel files

--- main.py
+++ main.py
@@ -2,18 +2,21 @@ import time
 from plyer import notification
 import requests
 import json
-import pandas as pd
-
+import openpyxl
 
 
 def readExel(path):
-    # xl_file = pd.ExcelFile(path)
-
-    df = pd.read_excel("Remind_data.xlsx", index_col=0)     
-    print(df.head()) # print the first 5 rows
-
-    # print(dfs)
-
+    # workbook object is created
+    wb_obj = openpyxl.load_workbook(path)
+    
+    sheet_obj = wb_obj.active
+    max_col = sheet_obj.max_column
+    
+    # Loop will print all columns name
+    for i in range(1, sheet_obj.max_row +1):
+        for i in range(1, max_col + 1):
+            cell_obj = sheet_obj.cell(row = 1, column = i)
+            print(cell_obj.value)
 
 
 def reminder(data):
