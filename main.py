caf9dd1516ae5295219255cb2d61360b63883b57
Brodevil <brodevil89@gmail.com>
2021-04-25 11:26:05 +0530

    Created the boiler plate

---
+++ main.py
@@ -0,0 +1,20 @@
+import time
+from plyer import notification
+import requests
+import json
+import xlrd
+
+
+def readExel(path):
+    pass
+
+
+def wordAPI(url):
+    pass
+
+def reminder(data):
+    pass
+
+
+if __name__ == "__main__":
+    pass
