e738b81650f7ff264caa1939c6390993f05ce2c8
Brodevil <brodevil89@gmail.com>
2021-04-26 16:26:01 +0530

    added in while loop and not putting the conditions

--- main.py
+++ main.py
@@ -25,12 +25,17 @@ def readExel(path):
 
 
 def reminder(data):
-    notification.notify(
-        title="Attempt 20-20-20 Eyes Exercise",
-        message="After every 20 minutes to give rest to your eyes see 20 meter long from your original distance in nature for 20 sec. Which defenatly safe the your eyes",
-        app_icon=r"E:\ADMIN\Pictures\Saved Pictures\abhinav.ico",
-        timeout=12
-    )
+    print(data)
+    while True:
+        for time, pourpose in data:
+            if time == datetime.time()
+
+        # notification.notify(
+        #     title="Attempt 20-20-20 Eyes Exercise",
+        #     message="After every 20 minutes to give rest to your eyes see 20 meter long from your original distance in nature for 20 sec. Which defenatly safe the your eyes",
+        #     app_icon=r"E:\ADMIN\Pictures\Saved Pictures\abhinav.ico",
+        #     timeout=12
+        # )
 
 
 def API(url):
