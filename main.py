e851f617dfa064ea861a08e82d6e220094911f57
Brodevil <brodevil89@gmail.com>
2021-04-26 15:46:41 +0530

    Started to code

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import openpyxl
 
 
 def readExel(path):
+    """Read the Exel fiel using openpyxl and return the 2d list as 1st will the time and 2nd will the pourpose """
     # workbook object is created
     wb_obj = openpyxl.load_workbook(path)
     
@@ -17,9 +18,7 @@ def readExel(path):
     # Loop will print all columns name
     for j in range(1, sheet_obj.max_row +1):
         for i in range(1, max_col + 1):
-            # row_obj = sheet_obj.cell(row =j , column = i)
             a = sheet_obj.cell(row=j, column=i)
-            print(a.value)
             records.append(a.value)
     records = [[records[i], records[i+1]] for i in range(0, len(records), 2)]
     return records
@@ -38,4 +37,6 @@ def wordAPI(url):
 
 
 if __name__ == "__main__":
-    readExel("M:\ADMIN\Critical Data\VS-Code\Reminder Application with Notification\Remind_data.xlsx")
+    # Giving the excel file's path as a parameters
+    exel = readExel("M:\ADMIN\Critical Data\VS-Code\Reminder Application with Notification\Remind_data.xlsx")  
+    print(exel)
